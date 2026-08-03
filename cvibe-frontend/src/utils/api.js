ad8c61bb4641c25d0f72f8a6e92febe15b2dba2c
cvibe <ramisatabassum1234@gmail.com>
2026-08-03 23:23:21 +0600

    fix: connect frontend to deployed backend

--- cvibe-frontend/src/utils/api.js
+++ cvibe-frontend/src/utils/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
+const API_URL = (
+  import.meta.env.VITE_API_URL || "http://localhost:5001"
+).replace(/\/$/, "");
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
